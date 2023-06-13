98015d6810c58e49b8e048cadbb50054b563af6d
Erick Lugo <ericklugoj@gmail.com>
2023-06-13 12:23:02 -0600

    feat: add test text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>PR DE PRUEBA</p>
         <a
           className="App-link"
           href="https://reactjs.org"
